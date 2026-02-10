fcaf8f001637d505b9e4fa6b574851b7f26a8f54
Hampus Edén <hampus.eden@gmail.com>
2026-02-10 19:37:51 +0100

    add timer and moving square example

--- examples/src/movingSquare.scala
+++ examples/src/movingSquare.scala
@@ -6,14 +6,9 @@ import scala.util.boundary, boundary.break
 import scala.collection.mutable.HashSet as MutSet
 import java.time.Duration
 import scala.util.Random
-
-object Colors:
-  val clear = (0, 0, 0, 0)
-  val square = (255, 255, 255, 255)
+import bl.time.Clock
 
 val keyPressed = MutSet.empty[Scancode]
-val fps = 60L
-val nanosPerFrame = 1_000_000_000L / fps
 
 object Square:
   extension (a: Boolean)
@@ -22,28 +17,49 @@ object Square:
       case (true, false)                 => 1
       case (false, true)                 => -1
 
-  object Key:
-    val (left, right, up, down) = (
-      Scancode.A,
-      Scancode.D,
-      Scancode.W,
-      Scancode.S
-    )
-
   val (w, h) = (50.0, 50.0)
   var (x, y) = (400.0, 400.0)
   val speed = 300.0
 
   def draw(r: Renderer): Unit =
-    r.drawColor = Colors.square
+    r.drawColor = (255, 255, 255, 255)
     r.fillRect(
       Rect(x - w / 2, y - h / 2, w, h)
     )
 
   def update(dt: Double): Unit =
-    x += dt * speed * (keyPressed(Key.right) - keyPressed(Key.left))
-    y += dt * speed * (keyPressed(Key.down) - keyPressed(Key.up))
-    Thread.sleep(Duration.ofNanos(Random.between(0L, 2000000L)))
+    x += dt * speed * (keyPressed(Scancode.D) - keyPressed(Scancode.A))
+    y += dt * speed * (keyPressed(Scancode.S) - keyPressed(Scancode.W))
+
+def mainLoop(r: Renderer)(using boundary.Label[Unit]): Unit =
+  val clock = Clock()
+  while true do
+    Event
+      .pollEvents()
+      .foreach:
+        case Event.Quit(_)                 => break()
+        case Event.Key.Down(scancode = sc) =>
+          keyPressed += sc
+        case Event.Key.Up(scancode = sc) =>
+          keyPressed -= sc
+        case _ =>
+
+    Square.update(clock.deltaDouble)
+
+    r.drawColor = (0, 0, 0, 0)
+    r.clear()
+
+    Square.draw(r)
+
+    r.drawColor = (255, 0, 0, 255)
+    r.renderScale = (5f, 5f)
+    r.renderDebugText(1f, 1f)(f"${clock.fps}%4.2f")
+    r.renderScale = (1f, 1f)
+
+    r.present()
+
+    clock.tick(60)
+end mainLoop
 
 @main
 def movingSquare(): Unit =
@@ -52,33 +68,7 @@ def movingSquare(): Unit =
     boundary:
       val (w, r) =
         bl.createWindowAndRenderer("Move Around!", 800, 800)
-
-      var past = System.nanoTime()
-      var dt = 0.0
-      while true do
-        Event
-          .pollEvents()
-          .foreach:
-            case Event.Quit(_)                 => break()
-            case Event.Key.Down(scancode = sc) =>
-              keyPressed += sc
-            case Event.Key.Up(scancode = sc) =>
-              keyPressed -= sc
-            case _ =>
-
-        Square.update(dt)
-
-        r.drawColor = Colors.clear
-        r.clear()
-
-        Square.draw(r)
-
-        r.present()
-
-        val now = System.nanoTime()
-        dt = (now - past).toDouble / 1e9
-        past = now
-      end while
+      mainLoop(r)
 
   finally
     bl.quit()
